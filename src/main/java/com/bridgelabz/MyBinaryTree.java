e54ab6f01c9e6e7bfa885f4c8043f9e74f4a0130
dada4747 <adsurerahul96@gmail.com>
2021-06-11 08:33:13 +0530

    ADDED:Ability to create a BST And add element

--- src/main/java/com/bridgelabz/MyBinaryTree.java
+++ src/main/java/com/bridgelabz/MyBinaryTree.java
@@ -7,5 +7,24 @@ public class MyBinaryTree<K extends Comparable<K>> {
         this.root = this.addRecursively(root, key);
     }
 
+    private BinaryNode<K> addRecursively(BinaryNode<K> current, K key) {
+        if (current == null)
+            return new BinaryNode<K>(key);
+        int compResult = key.compareTo(current.key);
+        if (compResult == 0)
+            return current;
+        if (compResult < 0) // if current>key -> left
+            current.left = addRecursively(current.left, key);
+        if (compResult > 0) // current<key ->right
+            current.right = addRecursively(current.right, key);
+        return current;
+    }
 
+    public int getSize() {
+        return this.getSizeRecursively(root);
+    }
+
+    private int getSizeRecursively(BinaryNode<K> current) {
+        return current == null ? 0 : 1 + this.getSizeRecursively(current.left) + this.getSizeRecursively(current.right);
+    }
 }
--- src/test/java/com/Bridgelabz/BinarySearchTest.java
+++ src/test/java/com/Bridgelabz/BinarySearchTest.java
@@ -5,5 +5,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BinarySearchTest {
-
+    @Test
+    public void given3Numbers_WhenAddedToBST_ShouldReturnSize3() {
+        MyBinaryTree<Integer> binaryTree = new MyBinaryTree<>();
+        binaryTree.add(56);
+        binaryTree.add(30);
+        binaryTree.add(70);
+        int size = binaryTree.getSize();
+        Assertions.assertEquals(3, size);
+    }
 }
