7780166328882fb75dfd642f2f2925b7ed4b0bf6
dada4747 <adsurerahul96@gmail.com>
2021-06-11 08:57:08 +0530

    ADDED:Create BST to add numbers of element in that

--- src/main/java/com/bridgelabz/MyBinaryTree.java
+++ src/main/java/com/bridgelabz/MyBinaryTree.java
@@ -2,7 +2,6 @@ package com.bridgelabz;
 
 public class MyBinaryTree<K extends Comparable<K>> {
     private BinaryNode<K> root;
-
     public void add(K key) {
         this.root = this.addRecursively(root, key);
     }
--- src/test/java/com/Bridgelabz/BinarySearchTest.java
+++ src/test/java/com/Bridgelabz/BinarySearchTest.java
@@ -14,4 +14,24 @@ public class BinarySearchTest {
         int size = binaryTree.getSize();
         Assertions.assertEquals(3, size);
     }
+    @Test
+    public void givenThreeNumbers_whenAddedToTree_ShouldReturnSizeThree(){
+        MyBinaryTree<Integer> customBinaryTree = new MyBinaryTree<>();
+        customBinaryTree.add(56);
+        customBinaryTree.add(30);
+        customBinaryTree.add(70);
+        customBinaryTree.add(22);
+        customBinaryTree.add(40);
+        customBinaryTree.add(60);
+        customBinaryTree.add(95);
+        customBinaryTree.add(11);
+        customBinaryTree.add(65);
+        customBinaryTree.add(3);
+        customBinaryTree.add(16);
+        customBinaryTree.add(63);
+        customBinaryTree.add(67);
+
+        int size = customBinaryTree.getSize();
+        Assertions.assertEquals(13,size);
+    }
 }
